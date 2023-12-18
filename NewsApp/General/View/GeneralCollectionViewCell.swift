ae087a426213b632f2ac5f7414a3da0082591a02
Artem <dogmaster31072003@gmail.com>
2023-12-18 03:45:43 +0200

    Created GeneralCollectionViewCell

---
+++ NewsApp/General/View/GeneralCollectionViewCell.swift
@@ -0,0 +1,21 @@
+//
+//  GeneralCollectionViewCell.swift
+//  NewsApp
+//
+//  Created by Melkor on 12/18/23.
+//
+
+import UIKit
+
+final class GeneralCollectionViewCell: UICollectionViewCell {
+    // MARK: - GUI Variables
+    
+    // MARK: - Initializations
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
