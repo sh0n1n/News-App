689de6cbd64b9ba34432cb4dbc9eab626aa833e5
Artem <dogmaster31072003@gmail.com>
2023-12-18 02:01:23 +0200

    Space ViewController created

---
+++ NewsApp/SpaceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceViewController.swift
+//  NewsApp
+//
+//  Created by Melkor on 12/17/23.
+//
+
+import UIKit
+
+final class SpaceViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
