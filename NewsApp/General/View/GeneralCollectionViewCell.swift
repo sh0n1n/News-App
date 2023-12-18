dbdfdb08fb6a86dd324b6f098e2df8a1745dd2dd
Artem <dogmaster31072003@gmail.com>
2023-12-18 03:49:59 +0200

    Added GUI Variables

--- NewsApp/General/View/GeneralCollectionViewCell.swift
+++ NewsApp/General/View/GeneralCollectionViewCell.swift
@@ -9,6 +9,29 @@ import UIKit
 
 final class GeneralCollectionViewCell: UICollectionViewCell {
     // MARK: - GUI Variables
+    private lazy var imageView: UIImageView = {
+       let view = UIImageView()
+        
+        return view
+    }()
+    
+    private lazy var blackView: UIView = {
+        let view = UIView()
+        
+        view.backgroundColor = .systemBackground
+        view.alpha = 0
+        
+        return view
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Title"
+        label.textColor = .systemBackground
+        
+        return label
+    }
     
     // MARK: - Initializations
     override init(frame: CGRect) {
