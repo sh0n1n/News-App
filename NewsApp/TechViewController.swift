c244ca7b9f944e81f3c5cb59d4601c208cdddee8
Artem <dogmaster31072003@gmail.com>
2023-12-18 02:01:33 +0200

    Technology ViewController created

---
+++ NewsApp/TechViewController.swift
@@ -0,0 +1,17 @@
+//
+//  TechViewController.swift
+//  NewsApp
+//
+//  Created by Melkor on 12/17/23.
+//
+
+import UIKit
+
+final class TechViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+    }
+}
