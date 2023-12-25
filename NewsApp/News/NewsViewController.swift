e78e7264e9e19421d41b41bcd6085ecd74430dfc
Artem <dogmaster31072003@gmail.com>
2023-12-25 12:37:06 +0200

    Created new VC

---
+++ NewsApp/News/NewsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  NewsViewController.swift
+//  NewsApp
+//
+//  Created by Melkor on 12/25/23.
+//
+
+import UIKit
+
+final class NewsViewController: UIViewController {
+    // MARK: - GUI Variables
+    private lazy var scrollView: UIScrollView = {
+        let view = UIScrollView()
+        
+        view.showsVerticalScrollIndicator = false
+        
+        return view
+    }()
+}
