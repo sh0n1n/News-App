468575b0427c1a5dddd09bd269f7746fbf8fb53a
Artem <dogmaster31072003@gmail.com>
2023-12-31 13:16:07 +0200

    Added all settings to news page

--- NewsApp/News/NewsViewController.swift
+++ NewsApp/News/NewsViewController.swift
@@ -26,4 +26,89 @@ final class NewsViewController: UIViewController {
         
         return view
     }()
+    
+    private lazy var dateLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "25.12.2023"
+        label.font = .systemFont(ofSize: 12)
+        label.textColor = .darkGray
+        
+        return label
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Some Title"
+        label.font = .systemFont(ofSize: 24)
+        label.textColor = .darkGray
+        
+        return label
+    }()
+    
+    private lazy var descriptionLabel: UILabel = {
+        let label = UILabel()
+        
+        label.text = "Very Long Description for News App Very Long Description for News App Very Long Description for News App Very Long Description for News App Very Long Description for News App Very Long Description for News App Very Long Description for News AppVery Long Description for News App Very Long Description for News App Very Long Description for News App Very Long Description for News App Very Long Description for News App Very Long Description for News App"
+        label.font = .systemFont(ofSize: 14)
+        label.textColor = .darkGray
+        label.numberOfLines = 0
+        
+        return label
+    }()
+    
+    // MARK: - mProperties
+    private let edgeInset = 10
+    
+    // MARK: - Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBackground
+        navigationController?.navigationBar.prefersLargeTitles = false
+        setupUI()
+    }
+    
+    // MARK: - Private methods
+    private func setupUI() {
+        scrollView.addSubview(contentView)
+        contentView.addSubview(imageView)
+        contentView.addSubview(dateLabel)
+        contentView.addSubview(titleLabel)
+        contentView.addSubview(descriptionLabel)
+        view.addSubview(scrollView)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        scrollView.snp.makeConstraints { make in
+            make .edges.equalToSuperview()
+        }
+        
+        contentView.snp.makeConstraints { make in
+            make.width.edges.equalToSuperview()
+        }
+        
+        imageView.snp.makeConstraints { make in
+            make.top.leading.trailing.equalToSuperview()
+            make.height.equalTo(view.snp.width)
+        }
+        
+        dateLabel.snp.makeConstraints { make in
+            make.top.equalTo(imageView.snp.bottom).offset(edgeInset)
+            make.leading.trailing.equalToSuperview().inset(edgeInset)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(dateLabel.snp.bottom).offset(edgeInset)
+            make.leading.trailing.equalToSuperview().inset(edgeInset)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(edgeInset)
+            make.leading.trailing.equalToSuperview().inset(edgeInset)
+            make.bottom.equalToSuperview().inset(edgeInset)
+        }
+    }
 }
