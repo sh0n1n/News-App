a9b0d2f5f92a9839cbbfc8c0ec9e361beec8222a
Artem <dogmaster31072003@gmail.com>
2023-12-31 15:47:09 +0200

    Added initialization and constraints

--- NewsApp/Space/DetailsCollectionViewCell.swift
+++ NewsApp/Space/DetailsCollectionViewCell.swift
@@ -43,5 +43,41 @@ final class DetailsCollectionViewCell: UICollectionViewCell {
         return label
     }()
     
+    // MARK: - Initializations
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupUI()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Methods
+    private func setupUI() {
+        addSubview(imageView)
+        addSubview(titleLabel)
+        addSubview(descriptionLabel)
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        imageView.snp.makeConstraints { make in
+            make.width.height.equalTo(self.frame.height)
+            make.top.leading.bottom.equalToSuperview()
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.trailing.equalToSuperview()
+            make.leading.equalTo(imageView.snp.trailing).offset(5)
+        }
+        
+        descriptionLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(5)
+            make.leading.equalTo(imageView.snp.trailing).offset(5)
+            make.trailing.bottom.equalToSuperview()
+        }
+    }
 }
