e7dc6328b72b7d5822876ece579d5985242e6cbc
Artem <dogmaster31072003@gmail.com>
2023-12-31 18:54:24 +0200

    Finished collectionView for 2-nd screen

--- NewsApp/Space/SpaceViewController.swift
+++ NewsApp/Space/SpaceViewController.swift
@@ -6,21 +6,101 @@
 //
 
 import UIKit
+import SnapKit
 
-final class SpaceViewController: UIViewController {
+class SpaceViewController: UIViewController {
     
     // MARK: - GUI Variables
-    
-    // MARK: - Properties
-    
+    private lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+       
+        
+        layout.minimumLineSpacing = 5
+        layout.minimumInteritemSpacing = 5
+        layout.sectionInset = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
+        
+                
+        let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height), collectionViewLayout: layout)
+        
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = .systemBackground
+        
+        return collectionView
+    }()
+        
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .systemBackground
+        setupUI()
+        collectionView.register(GeneralCollectionViewCell.self,
+                                forCellWithReuseIdentifier: "GeneralCollectionViewCell")
+        collectionView.register(DetailsCollectionViewCell.self,
+                                forCellWithReuseIdentifier: "DetailsCollectionViewCell")
     }
     
-    // MARK: - Methods
-    
     // MARK: - Private Methods
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        view.addSubview(collectionView)
+        
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        collectionView.snp.makeConstraints { make in
+            make.top.equalToSuperview()
+            make.leading.trailing.equalToSuperview().inset(5)
+            make.top.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+}
+
+// MARK: - UICollectionViewDataSource
+extension SpaceViewController: UICollectionViewDataSource {
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        section == 0 ? 1 : 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        var cell: UICollectionViewCell?
+        
+        if indexPath.section == 0 {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GeneralCollectionViewCell",
+                                                                for: indexPath) as? GeneralCollectionViewCell
+        } else {
+            cell = collectionView.dequeueReusableCell(withReuseIdentifier: "DetailsCollectionViewCell",
+                                                      for: indexPath) as? DetailsCollectionViewCell
+        }
+        return cell ?? UICollectionViewCell()
+    }
+    
+    
+}
+
+// MARK: - UICollectionViewDelegate
+extension SpaceViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView,
+                        didSelectItemAt indexPath: IndexPath) {
+        navigationController?.pushViewController(NewsViewController(),
+                                                 animated: true)
+    }
+}
+
+// MARK: - UICollectionViewDelegateFlowLayout
+extension SpaceViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectinViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = view.frame.width
+        let firstItemSize = CGSize(width: width, height: width)
+        let secondItemSize = CGSize(width: width, height: 100)
+        
+        return indexPath.section == 0 ? firstItemSize : secondItemSize
+    }
 }
