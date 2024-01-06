169aeca6f4c0dc08db5cb78077e766f5e80dfaea
Artem <dogmaster31072003@gmail.com>
2024-01-06 13:31:40 +0200

    Added Models

---
+++ NewsApp/General/Model/ArticleResponseObject.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleResponseObject.swift
+//  NewsApp
+//
+//  Created by Melkor on 1/6/24.
+//
+
+import Foundation
+
+struct ArticleResponseObject {
+    let title: String
+    let description: String
+    let urlToImage: String
+    let publishedAt: String
+}
---
+++ NewsApp/General/Model/NewsResponseObject.swift
@@ -0,0 +1,13 @@
+//
+//  NewsResponseObject.swift
+//  NewsApp
+//
+//  Created by Melkor on 1/6/24.
+//
+
+import Foundation
+
+struct NewsResponseObject {
+    let totalResults: Int
+    let articles: [String]
+}
