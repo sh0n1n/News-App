a024f9ee4d11076adcd7dc1702e44cc2e8ebfc87
Artem <dogmaster31072003@gmail.com>
2024-01-09 22:16:17 +0200

    Created APIManager

---
+++ NewsApp/Networking/APIManager.swift
@@ -0,0 +1,18 @@
+//
+//  APIManager.swift
+//  NewsApp
+//
+//  Created by Melkor on 1/9/24.
+//
+
+import Foundation
+
+final class APIManager {
+    private let apiKey = ""
+    private static let baseUrl = "GET https://newsapi.org/v2/"
+    private static let path = "everything"
+    
+    static func getNews(completion: @escaping ([ArticleResponseObject]) -> ()) {
+        
+    }
+}
