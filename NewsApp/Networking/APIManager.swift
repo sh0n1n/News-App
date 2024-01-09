78b538fc7d1002ecfce71671a1cc31643f31f224
Artem <dogmaster31072003@gmail.com>
2024-01-09 22:21:50 +0200

    Forming URL

--- NewsApp/Networking/APIManager.swift
+++ NewsApp/Networking/APIManager.swift
@@ -8,11 +8,13 @@
 import Foundation
 
 final class APIManager {
-    private let apiKey = ""
+    private static let apiKey = ""
     private static let baseUrl = "GET https://newsapi.org/v2/"
     private static let path = "everything"
     
     static func getNews(completion: @escaping ([ArticleResponseObject]) -> ()) {
+        let stringUrl = baseUrl + path + "?sources=nasa&language=en" + "&apiKey=\(apiKey)"
         
+        let url = URL(string: stringUrl)
     }
 }
