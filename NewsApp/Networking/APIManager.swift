ce44cc35ff31fd23f72e087da9f4b35320702bf1
Artem <dogmaster31072003@gmail.com>
2024-01-09 22:39:19 +0200

    Add handle response

--- NewsApp/Networking/APIManager.swift
+++ NewsApp/Networking/APIManager.swift
@@ -12,15 +12,29 @@ final class APIManager {
     private static let baseUrl = "GET https://newsapi.org/v2/"
     private static let path = "everything"
     
-    static func getNews(completion: @escaping ([ArticleResponseObject]) -> ()) {
+    // Create URL Path and make request
+    static func getNews(completion: @escaping (Result<[ArticleResponseObject], Error>) -> ()) {
         let stringUrl = baseUrl + path + "?sources=nasa&language=en" + "&apiKey=\(apiKey)"
         
-        let url = URL(string: stringUrl)
+        guard let url = URL(string: stringUrl) else { return }
         
         let session = URLSession.shared.dataTask(with: url) { data, response, error in
-            <#code#>
+            handleResponse(data: data, error: error, completion)
         }
         
         session.resume()
+    }
+    
+    // Handle response
+    private static func handleResponse(data: Data?,
+                                       error: Error?,
+                                       completion: @escaping (Result<[ArticleResponseObject], Error>) -> ()) {
+        if let error = error {
+            
+        } else if let data = data {
+            
+        } else {
+            
+        }
     }
 }
