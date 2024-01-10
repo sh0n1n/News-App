eb470b08851bac6fa1ba510d4e1069bf5171c43f
Artem <dogmaster31072003@gmail.com>
2024-01-10 23:17:47 +0200

    created speceViewModel from generalViewModel

---
+++ NewsApp/Space/ViewModel/SpaceViewModel.swift
@@ -0,0 +1,90 @@
+//
+//  SpaceViewModel.swift
+//  NewsApp
+//
+//  Created by Melkor on 1/10/24.
+//
+
+import Foundation
+
+protocol SpaceViewModelProtocol {
+    var reloadData: (() -> Void)? { get set }
+    var showError: ((String) -> Void)? { get set }
+    var reloadCell: ((Int) -> Void)? { get set }
+    
+    var numberOfCells: Int { get }
+    
+    func loadData()
+    func getArticle(for row: Int) -> ArticleCellViewModel
+    
+}
+
+final class SpaceViewModel: SpaceViewModelProtocol {
+    var reloadData: (() -> Void)?
+    var reloadCell: ((Int) -> Void)?
+    var showError: ((String) -> Void)?
+    
+    // MARK: - Properties
+    var numberOfCells: Int {
+        articles.count
+    }
+    
+    private var articles: [ArticleCellViewModel] = [] {
+        didSet {
+            DispatchQueue.main.async {
+                self.reloadData?()
+            }
+        }
+    }
+    
+    func getArticle(for row: Int) -> ArticleCellViewModel {
+        return articles[row]
+    }
+    
+     func loadData() {
+        print(#function)
+        
+        APIManager.getNews(from: .space) { [weak self] result in
+            guard let self = self else { return }
+            
+            switch result {
+            case .success(let articles):
+                self.articles = self.convertToCellViewModel(articles)
+                self.loadImage()
+            case .failure(let error):
+                DispatchQueue.main.async {
+                    self.showError?(error.localizedDescription)
+                }
+            }
+        }
+    }
+    
+    private func loadImage() {
+        for (index, article) in articles.enumerated() {
+            APIManager.getImageData(url: article.imageUrl) { [weak self] result in
+                
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let data):
+                        self?.articles[index].imageData = data
+                        self?.reloadCell?(index)
+                    case .failure(let error):
+                        self?.showError?(error.localizedDescription)
+                    }
+                }
+            }
+        }
+    }
+        
+        private func convertToCellViewModel(_ articles: [ArticleResponseObject]) -> [ArticleCellViewModel] {
+            return articles.map {
+                ArticleCellViewModel(article: $0)
+            }
+        }
+        
+        private func setupMockObjects() {
+            articles = [
+                ArticleCellViewModel(article: ArticleResponseObject(title: "First Object Title", description: "First Object Description", urlToImage: "...", date: "25.12.2023"))
+            ]
+        }
+    }
